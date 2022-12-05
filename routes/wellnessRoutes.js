05d62f6a3e96b5812731eee39c0d54d829cdc233
Ronnie Ineza <ronnie@Ronnies-MacBook-Pro.local>
2022-12-06 00:28:49 +0400

    grouped the routes into a get requests section and a post request section

--- routes/wellnessRoutes.js
+++ routes/wellnessRoutes.js
@@ -6,29 +6,30 @@ const controller = require('../controllers/wellnessController');
 const auth = require('../controllers/authController');
 
 router.get('/', controller.landing_page);
-router.get('/about', controller.about);
+router.get('/about', auth.verify, controller.about);
 router.get('/login', controller.login_page);
 router.get('/manager', auth.verify, auth.manager_page);
 router.get('/managerAdd', auth.verify, controller.managerAdd);
 router.get('/managerRemove', auth.verify, controller.managerRemove);
 router.post('/addStaff', auth.manager_add_user);
 router.get('/addGoal', auth.verify, controller.addGoal);
-router.post('/add_Goal', controller.add_Goal);
-router.get('/updateGoal', auth.verify, controller.updateGoal);
-router.post('/update_Goal', controller.update_Goal);
-router.post('/updateStaff', auth.updateStaff);
-router.post('/removeStaff', auth.removeStaff);
 router.get('/removeGoal', auth.verify, controller.removeGoal);
-router.post('/remove_Goal', controller.remove_Goal);
-router.get('/managerUpdate', auth.verify, controller.managerUpdate);
+router.get('/updateGoal', auth.verify, controller.updateGoal);
 router.get('/signup', controller.signUp_page);
 router.get('/fitness_goals', auth.verify, controller.fitness_goals);
 router.get('/nutrition_goals', auth.verify, controller.nutrition_goals);
 router.get('/mental_health_goals', auth.verify, controller.mental_health_goals);
-router.post('/staff_signup', auth.post_new_user);
-router.post('/staff_login', auth.login, auth.loggedIn_landing);
+router.get('/managerUpdate', auth.verify, controller.managerUpdate);
 router.get('/wellness', auth.verify, controller.wellness);
 router.get('/logout', auth.logout, controller.logout);
+
+router.post('/add_Goal', controller.add_Goal);
+router.post('/update_Goal', controller.update_Goal);
+router.post('/updateStaff', auth.updateStaff);
+router.post('/removeStaff', auth.removeStaff);
+router.post('/remove_Goal', controller.remove_Goal);
+router.post('/staff_signup', auth.post_new_user);
+router.post('/staff_login', auth.login, auth.loggedIn_landing);
 //error handling
 router.use(function (req, res) {
 	res.status(404);
